e73dffed2d8893f44b14f721966bd73e89ec9e7a
Nandini Bavana <106999973+NandiniSnowflake2@users.noreply.github.com>
2022-06-15 16:37:01 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,30 +17,36 @@ fruits_selected = streamlit.multiselect('Pick Some Fruits', list(my_fruitlist.in
 fruits_to_show = my_fruitlist.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_resonse = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalize = pandas.json_normalize(fruityvice_resonse.json())
+  return fruityvice_normalize
+
 streamlit.header('Fruitycive Fruit Advice')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?' )
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_resonse = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalize = pandas.json_normalize(fruityvice_resonse.json())
-    streamlit.dataframe(fruityvice_normalize)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+    
 except URLError as e:
   streamlit.error()
 
-
-
-
-#my_cur = my_cnx.cursor()
-#my_cur.execute("select * from fruit_load_list")
-#my_data_rows = my_cur.fetchall()
+streamlit.stop()
+    
 streamlit.header("The fruit load list contains:")
 #streamlit.dataframe(my_data_rows)
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list values('from streamlit')")
     return "Thanks for adding"+ new_fruit
+
+ if streamlit.button('Add a fruit to the list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the list'):
